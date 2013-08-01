ac0d0f06c103ec29ab90522f63458f1270b87f2f
rarch <rarcher496@gmail.com>
2013-08-01 17:33:54 -0400

    eleven

--- nine.py
+++ nine.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+
+# A Pythagorean triplet is a set of three natural numbers, a  b  c, for which,
+# a^2 + b^2 = c^2
+# For example, 32 + 42 = 9 + 16 = 25 = 52.
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
+
 ispythagorean=lambda a,b,c:a**2 + b**2 == c**2
 
 for a in xrange(1,1000):
