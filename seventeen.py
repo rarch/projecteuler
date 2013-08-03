79967720e7a82891aee967c2a2bbcdaf05ab3364
rarch <rarcher496@gmail.com>
2013-08-03 16:25:52 -0400

    less than ideal solution to zero problem

--- seventeen.py
+++ seventeen.py
@@ -4,6 +4,7 @@
 # If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
 # NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
 
+# numbers={0:"zero",1:"one",2:"two",3:"three",4:"four",5:"five",6:"six",7:"seven",8:"eight",9:"nine",10:"ten",\
 numbers={0:"",1:"one",2:"two",3:"three",4:"four",5:"five",6:"six",7:"seven",8:"eight",9:"nine",10:"ten",\
     11:"eleven",12:"twelve",13:"thirteen",14:"fourteen",15:"fifteen",16:"sixteen",17:"seventeen",\
     18:"eighteen",19:"nineteen",20:"twenty",30:"thirty",40:"forty",50:"fifty",60:"sixty",70:"seventy",\
@@ -14,21 +15,23 @@ numbers={0:"",1:"one",2:"two",3:"three",4:"four",5:"five",6:"six",7:"seven",8:"e
 thouPowers={0:"",1:"thousand",2:"million",3:"billion",4:"trillion",5:"quadrillion",6:"quintillion",7:"sextillion",\
     8:"septillion",9:"octillion"}
 
-REALLY='and'
+REALLY="and" # is that really necessary? only for decimals, right?
 
 def upto99(n):
     if n in numbers.keys():
         return numbers[n]
-    return ' '.join([numbers[(n/10)*10],numbers[n%10]])
+    return " ".join([numbers[(n/10)*10],numbers[n%10]])
 
 def upto999(n):
     if n<100:
         return upto99(n)
     if not n%100:
-        return ' '.join([numbers[n/100],numbers[100]])
-    return ' '.join([numbers[n/100],numbers[100], REALLY ,upto99(n%100)]) 
+        return " ".join([numbers[n/100],numbers[100]])
+    return " ".join([numbers[n/100],numbers[100], REALLY ,upto99(n%100)]) 
 
-def numToWord(n):
+def nonZeroToWord(n):
+    if n<0:
+        return " ".join(['negative',nonZeroToWord(-n)])
     if n<1000:
         return upto999(n)
 
@@ -38,13 +41,16 @@ def numToWord(n):
         divisor=divisor*1000
         i=i+1
 
-    return ' '.join([upto999((n/divisor)),\
+    return " ".join([upto999((n/divisor)),\
                      thouPowers[i],\
-                     numToWord(n-divisor*(n/divisor))])
+                     nonZeroToWord(n-divisor*(n/divisor))])
+
+def numToWord(n):
+    return nonZeroToWord(n) if n else "zero"
 
 s=0
 for i in xrange(1,1001):
     # print numToWord(i)
-    s=s+len(numToWord(i).replace(' ',''))
+    s=s+len(numToWord(i).replace(" ",""))
 
 print s
