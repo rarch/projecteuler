0b48cfe342af17a5fbe202ee369d2e5f1ffc141d
rarch <rarcher496@gmail.com>
2013-08-08 16:06:07 -0400

    refactored import

--- three/three.rb
+++ three/three.rb
@@ -3,7 +3,10 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
 
-require File.join(File.dirname(__FILE__), '../util', 'util')
+UTIL="util"
+UTIL_DIR="../#{UTIL}"
+
+require File.join(File.dirname(__FILE__), "#{UTIL_DIR}", "#{UTIL}")
 include Util
 
 N = 600851475143
