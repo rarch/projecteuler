e26246da3b9fcdf3252416f242809cd942892146
rarch <rarcher496@gmail.com>
2013-08-08 21:50:00 -0400

    piped some output to null file for cleaner appearance

--- test/test.sh
+++ test/test.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 # will test all project euler problems and makefiles
-PROJECT=$PWD/".."
-TEST=$PROJECT/"test"
-PROBS=434
 
+TESTHOME=$PWD
+PROJECT=$TESTHOME/".."
+PROBS=434
 EXTENSIONS=( "py" "rb" )
 DOTHS=".hs"
-
 HSSUFF="_hs.out"
 MAKEFILE="Makefile"
+PRE=">>>\t"
 
 directories=()
 
-echo "getting directory list"
+echo -e "${PRE}GETTING LIST OF DIRECTORIES"
 # go through all project euler problems 1..434, convert number to string
 for i in $(seq 1 $PROBS)
 do
-    subdir=$($TEST/numword.py $i)
+    subdir=$($TESTHOME/numword.py $i)
     if [ -d $PROJECT/$subdir ]; then
         directories+=("$subdir")
     fi
 done
-echo "got directory list"
+echo -e "${PRE}GOT"
 
+echo -e "${PRE}TESTING FILES"
 for subdir in ${directories[@]}
 do
     echo
@@ -47,16 +48,19 @@ do
         fi
     done
 
+    # test haskell files if there is a makefile
     if [ -f $PROJECT/$subdir/$MAKEFILE ]; then
         cd $PROJECT/$subdir
 
         filebasename=$(basename "$PROJECT/$subdir/$subdir$DOTHS")
         if [ -f $PROJECT/$subdir/$subdir$DOTHS ]; then
 
-            echo
+            # echo
+            # echo -e "${PRE}MAKING $HSSUFF FROM $DOTHS"
             make rm -s
-            make -s
+            make -s > /dev/null 2>&1 # do not print messy output from makefile
             make clean -s
+            # echo -e "${PRE}MADE"
 
             output=$($PROJECT/$subdir/$subdir$HSSUFF) #should check output against value, get this from README
             
@@ -65,7 +69,8 @@ do
             echo -ne " "$filebasename
             echo -ne "="$output
         fi
-        cd $TEST
+        cd $TESTHOME
     fi
     echo
 done
+echo -e "${PRE}COMPLETE"
