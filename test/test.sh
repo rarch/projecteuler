16b4aeded20782860058c05720476b32cb498103
rarch <rarcher496@gmail.com>
2013-08-08 21:23:21 -0400

    test.sh runs all files

--- test/test.sh
+++ test/test.sh
@@ -5,55 +5,67 @@ TEST=$PROJECT/"test"
 PROBS=434
 
 EXTENSIONS=( "py" "rb" )
+DOTHS=".hs"
 
 HSSUFF="_hs.out"
 MAKEFILE="Makefile"
 
-control_c() {
-    exit -1
-}
+directories=()
 
-trap control_c SIGINT
+echo "getting directory list"
 # go through all project euler problems 1..434, convert number to string
 for i in $(seq 1 $PROBS)
 do
-#   fail if any subsequent steps fail
     subdir=$($TEST/numword.py $i)
-#   check if projecteuler directory contains subdir with that name, and cd to it
     if [ -d $PROJECT/$subdir ]; then
-        echo $subdir":"
-#       in subdir check for executable files that start with same name with extension py, rb, and run them
-        for file in $PROJECT/$subdir/$subdir*
-        do
-            filebasename=$(basename "$file")
-            ext="${filebasename##*.}"
-            # filename="${filebasename%.*}"
-
-            if [ -x $file ]; then
-                for acceptable in ${EXTENSIONS[@]}
-                do
-                    if [ "$ext" = "$acceptable" ]; then
-                    # && [ "$filename" = "$subdir" ]; then
-
-                    # check for hs file, check for makefile
-                    # check make rm haskell, make haskell, clean haskell, proper output
-                    # make rm haskell
-                    
-                        output=$($file)
-                        echo -ne " "$filebasename
-                        echo -ne "="$output
-                    fi
-                done
-            fi
-        done
-        echo
-#       check for makefile
-
-#           make, check output
-#           make clean, check output
-#           check for executable files that end in _hs.out, and run it, print labeled output
-#           make rm, check output
-        echo
+        directories+=("$subdir")
     fi
 done
-#   exit
+echo "got directory list"
+
+for subdir in ${directories[@]}
+do
+    echo
+    echo $subdir":"
+
+    for file in $PROJECT/$subdir/$subdir*
+    do
+        filebasename=$(basename "$file")
+        ext="${filebasename##*.}"
+        # filename="${filebasename%.*}"
+
+        if [ -x $file ]; then
+            for acceptable in ${EXTENSIONS[@]}
+            do
+                if [ "$ext" = "$acceptable" ]; then
+                # && [ "$filename" = "$subdir" ]; then
+                    output=$($file) #should check output against value, get this from README
+                    echo -ne " "$filebasename
+                    echo -ne "="$output
+                fi
+            done
+        fi
+    done
+
+    if [ -f $PROJECT/$subdir/$MAKEFILE ]; then
+        cd $PROJECT/$subdir
+
+        filebasename=$(basename "$PROJECT/$subdir/$subdir$DOTHS")
+        if [ -f $PROJECT/$subdir/$subdir$DOTHS ]; then
+
+            echo
+            make rm -s
+            make -s
+            make clean -s
+
+            output=$($PROJECT/$subdir/$subdir$HSSUFF) #should check output against value, get this from README
+            
+            make rm -s
+
+            echo -ne " "$filebasename
+            echo -ne "="$output
+        fi
+        cd $TEST
+    fi
+    echo
+done
