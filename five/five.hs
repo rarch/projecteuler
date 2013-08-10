58fdac91e74ee3e461a0ff176220d46a2be5ce57
rarch <rarcher496@gmail.com>
2013-08-10 11:25:23 -0400

    haskell solution to five

---
+++ five/five.hs
@@ -0,0 +1,10 @@
+
+--2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+--What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+five :: Int
+five = foldr1 lcm [1..20]
+
+main = do
+    let result = five
+    print result
